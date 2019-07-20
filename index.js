cd3abf29e962c06cc27fa73dba541495146bef61
qui <qui021098@gmail.com>
2019-07-20 18:01:28 +0700

    deloy

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('fsfs');
+var express = require("express");
+
+var app =express();
+
+
+app.get('/',(req,res)=>{
+    res.end('helo qui');
+})
+
+app.listen(3000,()=>{
+ console.log("qui");
+});

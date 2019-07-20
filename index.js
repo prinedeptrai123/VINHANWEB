d2c175a1ecff02acbade8d1309fff4bd1d149858
qui <qui021098@gmail.com>
2019-07-20 18:57:35 +0700

    change all

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require("express");
+const path = require('path');
+
 
 var app =express();
+//express.static(root, [options]);
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/',(req,res)=>{
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 
 app.listen(process.env.PORT || 3000,()=>{

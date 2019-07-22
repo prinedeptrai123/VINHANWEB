5b22c2607c1caa03c65ff0580b2e7196eea72d24
qui <qui021098@gmail.com>
2019-07-22 21:11:32 +0700

    qui

--- public/js/my-js.js
+++ public/js/my-js.js
@@ -2,11 +2,16 @@ $('.full-image').on( "click", function() {
     
     var id = $(this).attr('id');
 
-    displayImage(id);
+   displayImage(id);
     $('#modal_luan').modal('show')
 });
 
 //set 
 function displayImage(id){
+    $(`li`).removeClass("active");
+    $(`.carousel-item`).removeClass("active");
+
+    $(`li[data-slide-to='${id}']`).addClass("active");
+    $(`.carousel-item[id='${id}']`).addClass("active");
     
 }

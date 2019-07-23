287e937fe353a9a75ae453c33b4843b7884b3916
qui <qui021098@gmail.com>
2019-07-23 10:06:24 +0700

    change

--- public/js/my-js.js
+++ public/js/my-js.js
@@ -1,5 +1,8 @@
 
 
+//Mảng hình ảnh xây cầu
+var arr_xay_cau = ['https://imgur.com/uONKmEl'];
+
 $(document).ready(function () {
 
     $(document).on("click",".img-activity",function() {
@@ -34,9 +37,12 @@ function displayImage(id) {
 //add function
 function createImage() {
     for (var i = 0; i < 15; i++) {
+
+        var url = createURL(i);
+
         var img = `<div class="col-lg-3 col-md-6 portfolio-item filter-xay-cau">
                         <div>
-                            <img id="0" class="full-image img-activity" src="/img/activity/${i}.jpg" alt="">
+                            <img id="0" class="full-image img-activity" src="${url}" alt="">
                             <div class="details">
                                 <span>19/07/2019</span>
                             </div>
@@ -44,4 +50,9 @@ function createImage() {
                     </div>`;
         $(".team-activity").append(img);
     }
+}
+
+//Tạo url 
+function createURL(id){
+    return `https://firebasestorage.googleapis.com/v0/b/oderfood-cf526.appspot.com/o/activityimage%2Fxaycau%2F${id}.JPG?alt=media&token=240c764f-65c4-48cb-97c3-ae45a2b8e23f`;
 }

d2a20ea2f199581b94cf3f2da43a58ce5c95b967
qui <qui021098@gmail.com>
2019-07-23 08:16:08 +0700

    add activity

--- public/js/my-js.js
+++ public/js/my-js.js
@@ -1,17 +1,40 @@
-$('.full-image').on( "click", function() {
-    
+
+
+$(document).ready(function () {
+    createImage();
+});
+
+$('.full-image').on("click", function () {
+
     var id = $(this).attr('id');
 
-   displayImage(id);
+    displayImage(id);
     $('#modal_luan').modal('show')
 });
 
 //set 
-function displayImage(id){
+function displayImage(id) {
     $(`li`).removeClass("active");
     $(`.carousel-item`).removeClass("active");
 
     $(`li[data-slide-to='${id}']`).addClass("active");
     $(`.carousel-item[id='${id}']`).addClass("active");
-    
+
+}
+
+
+
+//add function
+function createImage() {
+    for (var i = 0; i < 15; i++) {
+        var img = `<div class="col-lg-3 col-md-6 portfolio-item filter-xay-cau">
+                        <div>
+                            <img id="0" class="full-image" src="/img/activity/${i}.jpg" alt="">
+                            <div class="details">
+                                <span>19/07/2019</span>
+                            </div>
+                        </div>
+                    </div>`;
+        $(".team-activity").append(img);
+    }
 }
